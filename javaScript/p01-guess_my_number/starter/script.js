15ce56bcf929bc145b3b2eb6b9d06be107c3b952
Joelson Junior <joelson.ejr@gmail.com>
2023-11-27 21:01:41 -0300

    Removed unecessary comments

--- javaScript/p01-guess_my_number/starter/script.js
+++ javaScript/p01-guess_my_number/starter/script.js
@@ -1,18 +1,5 @@
 'use strict';
 
-/* console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct number';
-
-document.querySelector('.number').textContent = 13;
-
-document.querySelector('.score').textContent = 20;
-
-document.querySelector('.guess').value;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value); */
-
 let score = 20;
 let highscore = 0;
 let secretNumber;
