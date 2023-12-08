e2fd708e0b19587fa4640a37e20badd0b2c32113
Joelson Junior <joelson.ejr@gmail.com>
2023-12-08 14:49:49 -0300

    Finished lesson about this keyword

--- javaScript/js-behind-the-scenes/script.js
+++ javaScript/js-behind-the-scenes/script.js
@@ -1,32 +1,73 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2023 - birthYear;
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+// function calcAge(birthYear) {
+//   const age = 2023 - birthYear;
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1982 && birthYear <= 1996) {
-      var millenial = true;
-      // Creating NEW variable with same name as outer scope's variable.
-      const firstName = 'Joelson';
+//     if (birthYear >= 1982 && birthYear <= 1996) {
+//       var millenial = true;
+//       // Creating NEW variable with same name as outer scope's variable.
+//       const firstName = 'Joelson';
 
-      //Reassigning outer scope's variable
-      output = `NEW OUTPUT`;
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//       //Reassigning outer scope's variable
+//       output = `NEW OUTPUT`;
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    console.log(millenial);
-  }
-  printAge();
-  return age;
-}
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     console.log(millenial);
+//   }
+//   printAge();
+//   return age;
+// }
 
-const firstName = 'Joe';
+// const firstName = 'Joe';
+// calcAge(1982);
+
+console.log(this);
+
+//Regular functions receive their own values for "this"
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
 calcAge(1982);
 
-// console.log(age);
+//arrow function don't receive a "this" keyword. What happens it that
+// it receives the "this value" from the parent scope.
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1982);
+
+//this referencing to the object who called the method.
+const joe = {
+  year: 1982,
+  calcAge: function () {
+    console.log(this);
+    console.log(2023 - this.year);
+  },
+};
+
+//Here, the objetc joe is calling the method calcAge. That's why
+//"this" is pointing to "joe".
+joe.calcAge();
+
+const raquel = {
+  year: 1987,
+};
+
+raquel.calcAge = joe.calcAge;
+
+raquel.calcAge();
+
+const testFunction = raquel.calcAge;
+testFunction();
